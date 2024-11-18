63beea0e338ebbbe9cdb3ae5a2142d2ac474cf98
Hung Muhamath <hung.muhamath@flosports.tv>
2024-11-17 22:36:48 -0700

    change to stream

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,7 +1,18 @@
-import sharp from 'sharp';
 import axios from 'axios';
+import sharp from 'sharp';
 
 export const processImage = async (url: string, width: number, height: number): Promise<Buffer> => {
-  const response = await axios.get(url, { responseType: 'arraybuffer' });
-  return await sharp(response.data).resize(width, height).toBuffer();
+  const response = await axios({ url, method: 'GET', responseType: 'stream' });
+  
+  // Pipe the stream directly to sharp, which will process the image as it is being streamed.
+  return new Promise((resolve, reject) => {
+    const transformer = sharp().resize(width, height);
+    const chunks: Buffer[] = [];
+    
+    response.data
+      .pipe(transformer)
+      .on('data', (chunk: Buffer) => chunks.push(chunk))
+      .on('end', () => resolve(Buffer.concat(chunks))) 
+      .on('error', reject);  // Reject if there's an error during streaming
+  });
 };
